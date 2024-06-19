af7e583039e289c2dab255c5d668ae362859d6ae
eduperezn <edu.perezn.p@gmail.com>
2024-06-19 14:50:38 -0600

    working with geters and setters, pakages, OOP etc

--- src/Pelicula.java
+++
@@ -1,6 +0,0 @@
-public class Pelicula {
-    String nombre;
-    int fechaDeLanzamiento;
-    int duracionEnMinutos;
-    boolean incluidoEnElPlan;
-}
--- src/Principal.java
+++ src/Principal.java
@@ -1,20 +1,20 @@
+import dev.eduperezn.screenmatch.modelos.Pelicula;
+
 public class Principal {
     public static void main(String[] args) {
 
         Pelicula miPelicula = new Pelicula();
-        miPelicula.nombre = "Encanto";
-        miPelicula.fechaDeLanzamiento = 2021;
-        miPelicula.duracionEnMinutos = 120;
+        miPelicula.setNombre("Encanto");
+        miPelicula.setFechaDeLanzamiento(2021);
+        miPelicula.setDuracionEnMinutos(120);
+        miPelicula.setIncluidoEnElPlan(true);
 
-        System.out.println("Mi pelicula es: "+miPelicula.nombre);
-        System.out.println("Su fecha de lanzamiento es: "+miPelicula.fechaDeLanzamiento);
+        miPelicula.muestraFichaTecnica();
+        miPelicula.evalua(10);
+        miPelicula.evalua(10);
+        miPelicula.evalua(9);
+        System.out.println(miPelicula.getTotalDeLasEvaluaciones());
+        System.out.println(miPelicula.calculaMeida());
 
-        Pelicula otraPelicula = new Pelicula();
-        otraPelicula.nombre = "Matrix";
-        otraPelicula.fechaDeLanzamiento = 1999;
-        otraPelicula.duracionEnMinutos = 260;
-
-        System.out.println(miPelicula);
-        System.out.println(otraPelicula);
     }
 }
